461df7b47714be1f9e3872b93adaf746d07ffd5f
andr3felipe <andre.felipe_680@hotmail.com>
2024-01-29 20:44:07 -0300

    add: card

--- app/pokemon/[...name]/page.tsx
+++ app/pokemon/[...name]/page.tsx
@@ -1,4 +1,6 @@
+import { Button } from "@/app/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 interface PokemonProps {
   params: {
@@ -44,35 +46,92 @@ interface Pokemon {
   sprites: sprite;
 }
 
-async function fetchPokemon(name: string): Promise<Pokemon> {
+async function fetchPokemon(name: string): Promise<Pokemon | null> {
   const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`);
 
   if (!res.ok) {
-    throw new Error("Something went wrong");
+    return null;
   }
 
   return await res.json();
 }
 
 export default async function Pokemon({ params }: PokemonProps) {
-  const pokemon = await fetchPokemon(params.name);
-
-  const { name, height, weight, types, stats, moves, sprites } = pokemon;
+  const pokemon = (await fetchPokemon(params.name)) ?? null;
 
   return (
-    <main className="bg-background h-screen w-screen flex items-center justify-center">
-      <div className="bg-primary-foreground p-4 border-secondary">
-        <h1 className="text-primary font-semibold text-2xl capitalize">
-          {name}
-        </h1>
-        <Image
-          src={sprites.other["official-artwork"].front_default}
-          width={250}
-          height={250}
-          alt={name}
-          priority
-        />
-      </div>
+    <main className="bg-background min-h-screen min-w-screen flex items-center justify-center bg-slate-100">
+      {pokemon ? (
+        <div className="bg-primary-foreground border-slate-600  sm:p-10 p-4 rounded-lg border-2 b-5 flex flex-col items-center justify-center gap-8">
+          <div className="text-center">
+            <h1 className="text-primary font-semibold text-2xl capitalize mb-2">
+              {pokemon.name}
+            </h1>
+
+            <div>
+              <h2 className="inline  font-semibold text-red-800">Type: </h2>
+              {pokemon.types.map((type, index) => (
+                <p key={index} className="inline capitalize font-medium">
+                  {type.type.name}
+                </p>
+              ))}
+            </div>
+          </div>
+
+          <Image
+            className="border-2"
+            src={pokemon.sprites.other["official-artwork"].front_default}
+            width={pokemon.height * 20}
+            height={pokemon.height * 20}
+            alt={pokemon.name}
+            priority
+          />
+
+          <div className="flex gap-4 divide-x-2">
+            <div>
+              <p className="font-semibold text-red-800 inline">Height: </p>
+              <p className="font-medium inline">{pokemon.height}</p>
+            </div>
+            <div>
+              <p className="font-semibold text-red-800 inline ml-4">Weight: </p>
+              <p className="font-medium inline">{pokemon.weight}</p>
+            </div>
+          </div>
+
+          <div className="flex gap-8 items-start justify-center w-[100%]">
+            <div className="">
+              <h2 className="font-semibold text-red-800">Status:</h2>
+              {pokemon.stats.map((stat, index) => (
+                <div key={index}>
+                  <p className="inline capitalize font-medium">
+                    {stat.stat.name}:
+                  </p>
+                  <p className="inline"> {stat.base_stat}</p>
+                </div>
+              ))}
+            </div>
+
+            <div className="">
+              <h2 className="font-semibold text-red-800">Moves:</h2>
+              {pokemon.moves.slice(0, 6).map((move, index) => (
+                <p key={index} className="capitalize font-medium">
+                  {move.move.name}
+                </p>
+              ))}
+            </div>
+          </div>
+          <Button>
+            <Link href="/">Go Back</Link>
+          </Button>
+        </div>
+      ) : (
+        <div className="flex items-center justify-center gap-4 flex-col">
+          <h1 className="font-bold text-2xl">Pokémon not found.</h1>
+          <Button>
+            <Link href="/">Go Back</Link>
+          </Button>
+        </div>
+      )}
     </main>
   );
 }
